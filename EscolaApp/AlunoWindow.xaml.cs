0540081ba1377e57ed56e132061a775c0ec40cc9
Pedro Henrique Custodio de Lima <lima.custodio@academico.ifrn.edu.br>
2023-01-13 15:29:29 -0300

    aluno

--- EscolaApp/AlunoWindow.xaml.cs
+++ EscolaApp/AlunoWindow.xaml.cs
@@ -23,5 +23,67 @@ namespace EscolaApp
         {
             InitializeComponent();
         }
+        private void InserirClick(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            string Nome = txtNome.Text;
+            string Mat = txtMatricula.Text;
+            string Email = txtEmail.Text;
+            Aluno t = new Aluno()
+            {
+                Id = id,
+                Nome = Nome,
+                Matricula = Mat,
+                Email = Email,
+            };
+            NAluno.Inserir(t);
+            ListarClick(sender, e);
+        }
+        private void ListarClick(object sender, RoutedEventArgs e)
+        {
+            listAlunos.ItemsSource = null;
+            listAlunos.ItemsSource = NAluno.Listar();
+        }
+
+        private void AtualizarClick(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            string Nome = txtNome.Text;
+            string Mat = txtMatricula.Text;
+            string Email= txtEmail.Text;
+            Aluno t = new Aluno()
+            {
+                Id = id,
+                Nome = Nome,
+                Matricula = Mat,
+                Email = Email,
+            };
+            NAluno.Atualizar(t);
+            ListarClick(sender, e);
+        }
+
+        private void ExcluirClick(object sender, RoutedEventArgs e)
+        {
+            if (listAlunos.SelectedItem != null)
+            {
+
+                NTurma.Excluir((Turma)listAlunos.SelectedItem);
+                ListarClick(sender, e);
+            }
+            else
+                MessageBox.Show("Selecione o aluno a ser excluida");
+        }
+
+        private void listAlunos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listAlunos.SelectedItem != null)
+            {
+                Aluno obj = (Aluno)listAlunos.SelectedItem;
+                txtId.Text = obj.Id.ToString();
+                txtNome.Text = obj.Nome;
+                txtMatricula.Text = obj.Matricula;
+                txtEmail.Text = obj.Email;
+            }
+        }
     }
 }
